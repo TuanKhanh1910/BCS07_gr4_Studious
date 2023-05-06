f79f758971f2aa95c23101304c2140b266da1f29
Lam_Diem_Kieu <kieulam6396@gmail.com>
2023-05-06 21:13:18 +0700

    chỉnh code lần cuối

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 30) {
+        document.getElementById("topHeader").classList.add("fix__top");
+    } else {
+        document.getElementById("topHeader").classList.remove("fix__top");
+    }
+};
+
+// dark and light
+document.getElementById("switchButton").onclick = function () {
+    document.getElementById("mybody").classList.toggle("dark");
+};
+
+document.getElementById("fa__bars").onclick = function () {
+    document.getElementById("fa__bars").style.display = "none";
+    document.getElementById("fa__close").style.display = "block";
+};
+document.getElementById("fa__close").onclick = function () {
+    document.getElementById("fa__bars").style.display = "block";
+    document.getElementById("fa__close").style.display = "none";
+};
